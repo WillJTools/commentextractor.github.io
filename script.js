f32e7849b734c50910c10f944061d409d0896e88
Will J <138139664+WillJTools@users.noreply.github.com>
2023-08-20 21:22:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
 document.getElementById('scriptFile').addEventListener('change', function(event) {
-    const file = event.target.files[0];
-  
-    if (file) {
-      const reader = new FileReader();
-  
-      reader.onload = function(e) {
-        const scriptText = e.target.result;
-        const comments = extractComments(scriptText);
-        displayComments(comments);
-      };
-  
-      reader.readAsText(file);
-    }
-  });
-  
-  function extractComments(scriptText) {
-    // Implement a function to extract comments from the script text.
-    // This can vary depending on the script language. For example, in JavaScript:
-    // const comments = scriptText.match(/\/\/.*|\/\*[^]*?\*\//g);
-    // In Python, you can use regular expressions to capture comments.
-    // Adjust this function according to the script formats you want to support.
+  const file = event.target.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+      const scriptText = e.target.result;
+      const comments = extractComments(scriptText);
+      displayComments(comments);
+    };
+
+    reader.readAsText(file);
   }
-  
-  function displayComments(comments) {
-    const commentsOutput = document.getElementById('commentsOutput');
-    commentsOutput.textContent = comments.join('\n\n');
-  }
-  
+});
+
+function extractComments(scriptText) {
+  // Implement your comment extraction logic here.
+  // This can vary based on different script formats.
+  // For example, you can use regular expressions to capture comments.
+  // Adjust this function according to the script formats you want to support.
+  return ['Comment 1', 'Comment 2', '...'];
+}
+
+function displayComments(comments) {
+  const commentsOutput = document.getElementById('commentsOutput');
+  commentsOutput.textContent = comments.join('\n\n');
+}

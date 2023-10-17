a8a94ae7920dd3b445ee533bbceb8c672d5ebbad
Will J <138139664+WillJTools@users.noreply.github.com>
2023-10-17 00:41:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,44 @@
 document.getElementById('scriptFile').addEventListener('change', function(event) {
-  const file = event.target.files[0];
+    const file = event.target.files[0];
 
-  if (file) {
-    const reader = new FileReader();
+    if (file) {
+        const reader = new FileReader();
 
-    reader.onload = function(e) {
-      const scriptText = e.target.result;
-      const fileExtension = getFileExtension(file.name);
-      const comments = extractComments(scriptText, fileExtension);
-      displayComments(comments, file.name); // Pass the file name
-    };
+        reader.onload = function(e) {
+            const scriptText = e.target.result;
+            const fileExtension = getFileExtension(file.name);
+            const comments = extractComments(scriptText, fileExtension);
+            displayComments(comments, file.name); // Pass the file name
+        };
 
-    reader.readAsText(file);
-  }
+        reader.readAsText(file);
+    }
 });
 
 function getFileExtension(filename) {
-  return filename.split('.').pop().toLowerCase();
+    return filename.split('.').pop().toLowerCase();
 }
 
 function extractComments(scriptText, fileExtension) {
-  if (fileExtension === 'js' || fileExtension === 'java' || fileExtension === 'cpp' || fileExtension === 'go') {
-    return scriptText.match(/\/\/.*|\/\*[^]*?\*\//g) || [];
-  } else if (fileExtension === 'py') {
-    return scriptText.match(/#.*$/gm) || [];
-  } else if (fileExtension === 'bash') {
-    return scriptText.match(/#.*$/g) || [];
-  } else if (fileExtension === 'rb') {
-    return scriptText.match(/#.*$/gm) || [];
-  }
+    if (fileExtension === 'sh' || fileExtension === 'bash') {
+        return scriptText.match(/#.*$/gm) || [];
+    } else if (fileExtension === 'js' || fileExtension === 'java' || fileExtension === 'cpp' || fileExtension === 'go') {
+        return scriptText.match(/\/\/.*|\/\*[^]*?\*\//g) || [];
+    } else if (fileExtension === 'py' || fileExtension === 'rb') {
+        return scriptText.match(/#.*$/gm) || [];
+    }
 
-  return [];
+    return [];
 }
 
 function displayComments(comments, fileName) {
-  const commentsOutput = document.getElementById('commentsOutput');
-  const fileNameElement = document.getElementById('fileName');
-  commentsOutput.textContent = comments.join('\n\n');
-  
-  if (fileName) {
-    fileNameElement.textContent = `Uploaded File: ${fileName}`;
-  } else {
-    fileNameElement.textContent = '';
-  }
+    const commentsOutput = document.getElementById('commentsOutput');
+    const fileNameElement = document.getElementById('fileName');
+    commentsOutput.textContent = comments.join('\n\n');
+
+    if (fileName) {
+        fileNameElement.textContent = `Uploaded File: ${fileName}`;
+    } else {
+        fileNameElement.textContent = '';
+    }
 }
